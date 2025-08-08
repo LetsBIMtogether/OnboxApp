8edf5af18cd5a1a031f40fa0667a83a2107ade56
LetsBIMtogether <letsbimtogether@gmail.com>
2025-08-08 03:08:00 -0400

    .NET8 URL ribbon button

--- ONBOXApp/About.cs
+++ ONBOXApp/About.cs
@@ -6,19 +6,37 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 using System.Xml;
+using System.Diagnostics;
 
 namespace ONBOXAppl
 {
     [Transaction(TransactionMode.Manual)]
     class SiteONBOX : IExternalCommand
     {
+
+
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+
+#if REVIT2025UP
+
+                var startInfo = new ProcessStartInfo {
+                    FileName = "http://www.onboxdesign.com.br/",
+                    UseShellExecute = true
+                };
+                Process.Start(startInfo);
+
+#else
+
             System.Diagnostics.Process.Start("http://www.onboxdesign.com.br/");
+
+#endif
+
             return Result.Succeeded;
         }
     }
